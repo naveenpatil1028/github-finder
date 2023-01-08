13770fa24d13be64422fe855b940fa7a95d59366
naveenpatil1028 <121613773+naveenpatil1028@users.noreply.github.com>
2023-01-08 18:42:21 +0530

    Reducer

--- src/Components/Users/UserResults.js
+++ src/Components/Users/UserResults.js
@@ -1,15 +1,13 @@
-import React, { useEffect,useContext } from 'react'
+import React, { useContext } from 'react'
 import GithubContext from '../../context/Github/GithubContext'
 import Spinner from '../Layout/Spinner'
 import UserItem from './UserItem'
 
 function UserResults() {
 
-    const {users,loading,fetchUser} = useContext(GithubContext)
+    const {users,loading} = useContext(GithubContext)
 
-    useEffect(() => {
-    fetchUser()
-    }, [])
+ 
 
 
 
--- src/context/Github/GithubContext.js
+++ src/context/Github/GithubContext.js
@@ -1,5 +1,5 @@
-import { createContext, useState } from "react";
-
+import { createContext, useReducer} from "react";
+import GithubReducer from "./GithubReducer";
 const GithubContext=createContext()
 
 const GITHUB_URL= "https://api.github.com"
@@ -8,11 +8,17 @@ const GITHUB_TOKEN="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"
 
 export const GithubProvider=({children})=>{
 
+    const initialState={
+        users:[],
+        loading:true
+    }
 
-    const [users, setUsers] = useState([])
-    const [loading, setLoading] = useState(true)
+    const [state, dispatch] = useReducer(GithubReducer,initialState)
+    // const [users, setUsers] = useState([])
+    // const [loading, setLoading] = useState(true)
 
     const fetchUser=async ()=>{
+        setLoading();
         const response=await fetch(`${GITHUB_URL}/users`,{
             headers:{
                 Authorization:`token ${GITHUB_TOKEN}`
@@ -21,13 +27,16 @@ export const GithubProvider=({children})=>{
 
         const data=await response.json()
 
-        setUsers(data);
-        setLoading(false)
+        dispatch({
+            type:'GET_USERS',
+            payload:data,
+        })
 }
 
+const setLoading=()=>dispatch({type: 'SET_LOADING'})
 return <GithubContext.Provider value={{
-        users,
-        loading,
+        users:state.users,
+        loading:state.loading,
         fetchUser
 }}>
     {children}
